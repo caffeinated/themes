b51906c31a5d20e6942f8dd38bb8a9dabb94f7c9
Shea Lewis <shea.lewis89@gmail.com>
2015-02-04 10:09:44 -0800

    Laravel-style config file

--- src/config/themes.php
+++ src/config/themes.php
@@ -1,9 +1,43 @@
 <?php
 
 return [
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Default Active Theme
+	|--------------------------------------------------------------------------
+	|
+	| Assign the default active theme to be used if one is not set during
+	| runtime.
+	|
+	*/
 
 	'active' => 'bootstrap',
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Templating Engine
+	|--------------------------------------------------------------------------
+	|
+	| Switch between using either Blade or Twig as youe templating engine. To
+	| use Twig, be sure to install the twigbridge package and register it's
+	| service provider BEFORE the Caffeinated Themes service provider.
+	|
+	*/
+
 	'engine' => 'blade',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Path to Themes
+	|--------------------------------------------------------------------------
+	|
+	| Define the path where you'd like to store your themes. Note that if you
+	| choose a path that's outside of your public directory, you will still need
+	| to store your assets (CSS, images, etc.) within your public directory.
+	|
+	*/
+
 	'path'   => public_path('themes'),
 
 ];
