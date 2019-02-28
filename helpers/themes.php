29255c43fe0f1b689b6c91a3bf821dff4f948d42
Ahmet Bora <byybora@gmail.com>
2019-02-28 12:44:47 +0300

    Update themes.php

--- helpers/themes.php
+++ helpers/themes.php
@@ -13,3 +13,20 @@ if (!function_exists('theme_path')) {
         return Theme::path($file, $theme);
     }
 }
+
+if (!function_exists('theme_asset')) {
+    /**
+     * Return the asset url to the given theme file.
+     *
+     * @param  string  $file
+     * @param  string  $theme
+     * @return string
+     * @author Ahmet Bora <byybora@gmail.com>
+     */
+    function theme_asset($file = '', $theme = null)
+    {
+        $theme = $theme ?? Theme::getCurrent();
+        
+        return asset("themes/{$theme}/{$file}");
+    }
+}
